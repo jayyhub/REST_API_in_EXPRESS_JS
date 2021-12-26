02ed3c5046d965b7d5b153033a86645aa63ae79b
Jawwad <mjawwad072@gmail.com>
2021-12-26 19:26:24 +0500

    Added DELETE  Method

--- app.js
+++ app.js
@@ -64,6 +64,20 @@ app.put('/api/courses/:id', (req, res) =>
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) =>
+{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+    {
+        res.status(400).send("Course Not Found");
+        return;
+    }
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+})
+
 //PORT 
 const port = process.env.PORT || 3000;
 app.listen(port, function()
