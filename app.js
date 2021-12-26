3dda82c20ce182d27d3394b22a53ea62cc889bdc
Jawwad <mjawwad072@gmail.com>
2021-12-26 18:03:30 +0500

    Added POST Method

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const app = express();
+
+app.use(express.json());
+
+const courses = 
+[
+    { id:1, name:"Course1" },
+    { id:2, name:"Course2" },
+    { id:3, name:"Course3" }
+];
 
 app.get('/', function(req, res) 
 {
@@ -9,12 +17,27 @@ app.get('/', function(req, res)
 
 app.get('/api/courses', (req, res) =>
 {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) =>
 {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)
+    {
+        res.status(404).send('THE COURSE DOES NOT EXIST');
+    }
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => 
+{
+    const course = { 
+        id: courses.length+1, 
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 app.get('/api/courses/:year/:month', (req, res) =>
